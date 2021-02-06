86c5f75ac6cd21212cd0ae79776a3c0519936737
Keshab Thanet <keshab.thanet0806@gmail.com>
2021-02-06 08:48:00 +0545

    another commit

---
+++ portfolio_project/blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Blog
+
+# Create your views here.
+def index(request):
+    blogs=Blog.objects.all()
+    return render(request,'blog/home.html',{'Blog':blogs})
